2643fedc5bacef4e4e57ec27aea523535fb61df4
hhotatea <hhotatea@gmail.com>
2019-08-19 21:40:15 +0900

    ぷｓｈ

---
+++ Assets/script/sampleMeshVertex.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sampleMeshVertex : MonoBehaviour
+{
+    public MeshFilter mMesh;
+    public int num=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3[] test = mMesh.mesh.vertices;
+        this.transform.position  = test[num];
+    }
+}
